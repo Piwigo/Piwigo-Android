c2ad3782ba8cfe85e3adad8ccc7aa8bc2dd35d71
Valentin BAIZEAU <valentin.baizeau@epitech.eu>
2019-09-02 12:46:15 +0200

    Some needed cleanup, and missing infos

--- app/src/main/java/org/piwigo/helper/URLHelper.java
+++ app/src/main/java/org/piwigo/helper/URLHelper.java
@@ -1,25 +1,15 @@
 package org.piwigo.helper;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.piwigo.ui.login.LoginActivity;
-import org.piwigo.ui.main.MainActivity;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.UnknownHostException;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLException;
-import javax.net.ssl.SSLHandshakeException;
-import javax.net.ssl.SSLPeerUnverifiedException;
 
 public class URLHelper extends AsyncTask<String, Void, String> {
 
