1fdb6175c4b48c32fed3bab20ee09defa3d95cda
Valentin BAIZEAU <valentin.baizeau@epitech.eu>
2019-12-13 19:39:16 +0100

    Chunk photos at upload (#214)
    closes #126
    
    * Default upload method is now chunking image (avoid getting blocked by the max_upload_size server config)
    * The app is now using the same chunk size as the form (given in pwg.session.getStatus method) to avoid any issues

--- app/src/main/java/org/piwigo/helper/NetworkHelper.java
+++ app/src/main/java/org/piwigo/helper/NetworkHelper.java
@@ -12,13 +12,33 @@ public class NetworkHelper {
         INSTANCE = this;
     }
 
-    public boolean hasInternet(Context context) {
+    public NetworkInfo getCurrentNetwork(Context context)
+    {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo currentNetwork = cm.getActiveNetworkInfo();
+        //The usage of getActiveNetworkInfo() is deprecated since API 28
+        //The proper way would be to use getActiveNetwork, but it is only supported since API 23
+        //So for now we may want to keep it like this
+        NetworkInfo network = cm.getActiveNetworkInfo();
+
+        return (network);
+    }
+
+    public boolean hasInternet(Context context) {
+        NetworkInfo currentNetwork = getCurrentNetwork(context);
 
         if (currentNetwork != null) {
             return (currentNetwork.isConnected());
         }
         return (false);
+    }
+
+    public int getNetworkType(Context context)
+    {
+        NetworkInfo currentNetwork = getCurrentNetwork(context);
+
+        if (currentNetwork != null) {
+            return (currentNetwork.getType());
+        }
+        return (-1); //The symbolic name for "0" is MOBILE, if we end this return it means that there is no internet connection so it should be -1
     }
 }
